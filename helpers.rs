6fa470db4ae451ea952eb17b8f782e11929cef92
dragonulq <mihnea.savin@gmail.com>
2025-05-20 19:34:05 +0100

    Added reading of library_name through the guest memory object when calling host_dlopen()

--- helpers.rs
+++ helpers.rs
@@ -3,7 +3,8 @@ use std::{
     path::{Path, PathBuf},
 };
 use std::collections::HashSet;
-use wasmtime::{Engine, Module, ExternType, ValType, Val};
+use wasmtime::{Engine, Module, Extern, Caller, Memory};
+use wasmtime_wasi::WasiP1Ctx;
 
 pub fn dependency_order(engine: &Engine, root: &Path) -> Result<Vec<(String, PathBuf)>> {
     dfs(engine, root)
@@ -16,7 +17,7 @@ fn dfs(engine: &Engine, path: &Path) -> Result<Vec<(String, PathBuf)>> {
 
     let mut list = Vec::<(String, PathBuf)>::new();
     for imp in module.imports() {
-        if imp.module() == "wasi_snapshot_preview1" || imp.module() == "env"  || imp.module() == "host"{
+        if imp.module() == "wasi_snapshot_preview1" || imp.module() == "env"  || imp.module() == "host" {
             continue;
         }
         let dep = path.with_file_name(format!("{}.wasm", imp.module()));
@@ -32,25 +33,6 @@ fn dfs(engine: &Engine, path: &Path) -> Result<Vec<(String, PathBuf)>> {
     Ok(list)
 }
 
-pub fn collect_env_imports(module: &Module) -> Vec<(String, ExternType)> {
-
-    module
-        .imports()
-        .filter(|imp| imp.module() == "env")
-        .map(|imp| (imp.name().to_string(), imp.ty().clone()))
-        .collect()
-}
-
-pub fn zero_for(ty: &ValType) -> Val {
-    match ty {
-        ValType::I32 => Val::I32(0),
-        ValType::I64 => Val::I64(0),
-        ValType::F32 => Val::F32(0),
-        ValType::F64 => Val::F64(0),
-        other => panic!("unsupported global type {other:?}"),
-    }
-}
-
 pub fn remove_duplicates(vec: Vec<(String, PathBuf)>) -> Vec<(String, PathBuf)> {
     let mut seen_strings = HashSet::new();
     vec.into_iter()
@@ -63,5 +45,12 @@ pub fn remove_duplicates(vec: Vec<(String, PathBuf)>) -> Vec<(String, PathBuf)> 
             }
         })
         .collect()
+}
+
+pub fn get_instance_memory_copy(caller: &mut Caller<'_, WasiP1Ctx>) -> Memory {
+    match caller.get_export("memory") {
+        Some(Extern::Memory(memory)) => memory.clone(),
+        _ => panic!("missing memory export!")
+    }
 }
 
--- wasm_launcher.rs
+++ wasm_launcher.rs
@@ -1,15 +1,14 @@
 use anyhow::{anyhow, Context, Result};
 use std::sync::{Mutex, OnceLock};
 use std::{env, fs, process};
-use wasmtime::{AsContextMut, Caller, Engine, Func, Linker, Module, Store, Instance, Val};
+use wasmtime::{AsContextMut, Caller, Engine, Func, Linker, Module, Store, Instance, Val, AsContext};
 use wasmtime_wasi::preview1::add_to_linker_sync;
 use wasmtime_wasi::I32Exit;
 use wasmtime_wasi::WasiCtxBuilder;
 use wasmtime_wasi::WasiP1Ctx;
 mod helpers;
 
-use helpers::dependency_order;
-use helpers::remove_duplicates;
+use helpers::{dependency_order, remove_duplicates, get_instance_memory_copy};
 
 struct GlobalWasmCtx {
     engine: Engine,
@@ -28,7 +27,7 @@ impl GlobalWasmCtx {
     }
 }
 
-fn global_objects() -> &'static GlobalWasmCtx {
+fn get_global_objects() -> &'static GlobalWasmCtx {
     static GLOBAL_OBJECTS: OnceLock<GlobalWasmCtx> = OnceLock::new();
     GLOBAL_OBJECTS.get_or_init(|| GlobalWasmCtx::new())
 }
@@ -49,8 +48,8 @@ fn main() -> Result<()> {
     };
 
     let argv: Vec<String> = env::args().skip(1).collect();
-    let GLOBAL_OBJECTS = global_objects();
-    let engine = &GLOBAL_OBJECTS.engine;
+    let global_objects = get_global_objects();
+    let engine = &global_objects.engine;
 
     let mut wasi_ctx_builder = WasiCtxBuilder::new();
     let wasi_ctx = (&mut wasi_ctx_builder)
@@ -60,38 +59,60 @@ fn main() -> Result<()> {
 
     let mut store = Store::new(&engine, wasi_ctx);
     let instance:Instance = {
-        let linker_guard = &mut GLOBAL_OBJECTS.linker.lock().unwrap();
+        let linker_guard = &mut global_objects.linker.lock().unwrap();
         let linker: &mut Linker<WasiP1Ctx> = &mut *linker_guard;
         add_to_linker_sync(linker, |t| t)?;
 
         let modules_to_be_instantiated = dependency_order(&engine, &wasm_path.as_path())?;
+        const LIBRARY_PATH_MAX_LENGTH: i32 = 4096;
         let dlopen_func = Func::wrap(
             store.as_context_mut(),
-            |mut caller: Caller<'_, WasiP1Ctx>, ptr: i32, flags: i32| -> i32 {
-                let GLOBAL_OBJECTS = global_objects();
-                let linker_guard = &mut GLOBAL_OBJECTS.linker.lock().unwrap();
+            |mut caller: Caller<'_, WasiP1Ctx>, ptr: i32, library_len: i32| -> i32 {
+                let global_objects = get_global_objects();
+                let linker_guard = &mut global_objects.linker.lock().unwrap();
                 let linker: &mut Linker<WasiP1Ctx> = &mut *linker_guard;
-
+                let memory = get_instance_memory_copy(&mut caller);
                 let store = caller.as_context_mut();
-                let engine = &GLOBAL_OBJECTS.engine;
-                let module = Module::from_file(engine, "test-dlopen.wasm").unwrap();
+                let engine = &global_objects.engine;
+                
+                let mut backing_array = [0u8; LIBRARY_PATH_MAX_LENGTH as usize];
+
+                if library_len > LIBRARY_PATH_MAX_LENGTH {
+                    panic!("Length of library to dlopen cannot be larger than {}!", LIBRARY_PATH_MAX_LENGTH);
+                }
+
+                let buffer: &mut [u8] = &mut backing_array[0..library_len as usize];
+
+                match memory.read(store.as_context(), ptr as usize, buffer) {
+                    Ok(()) => {},
+                    _ => panic!("Something went wrong while reading guest memory to get library name!")
+                }
+
+                let library_name = match std::str::from_utf8(&buffer[..library_len as usize]) {
+                    Ok(s) => s.to_string(),
+                    Err(_) => panic!("Could not convert buffer to string!"),
+                };
+                println!("Loaded library: {}", &library_name);
+                let module = Module::from_file(engine, &library_name).unwrap();
                 let instance = linker.instantiate(store, &module).unwrap();
-                let result;
-                let option_func = instance.get_func(caller.as_context_mut(), "mul_by_3");
-                let mut params = [Val::I32(177)];
+                
+                let option_func = instance.get_func(caller.as_context_mut(), "mul_by_3").unwrap();
+                let params = [Val::I32(177)];
                 let mut results:Vec<Val> = Vec::new();
                 results.push(Val::I32(0));
-                match  option_func{
-                    Some(func) => {result = func.call(caller.as_context_mut(), &params, &mut results)},
-                    None => {return -1;}
-                }
+
+                match option_func.call(caller.as_context_mut(), &params, &mut results) {
+                   Ok(()) => {},
+                   _ => panic!("Could not apply mul_by_3 into function!") 
+                };
+
                 match results[0] {
-                    Val::I32(531) => {println!("Got expected value back")}, 
+                    Val::I32(531) => {println!("Got expected value back")},
                     Val::I32(x) => {println!("Got {} back", x)},
                     _ => {}
-                    
+
                 }
-                println!("host_dlopen called with ptr={}, flags={}", ptr, flags);
+                println!("host_dlopen called with ptr={}, flags={}", ptr, library_len);
                 70
             },
         );
